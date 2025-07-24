21b63b207c278041b5f470aff7c8fa69614ed633
malisteee <malisteee@gmail.com>
2025-07-24 17:40:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const cont = document.getElementById("contenedor-malla");
 
 const semestres = [
   ["Química General y Orgánica", "Antropología", "Introducción a la Tecnología Médica", "Biología Celular", "Matemáticas Básica", "Integrado en Habilidades Científicas"],
-  ["Bioquímica General","Morfología Básica","Ética","Tecnología Médica en el Equipo de Salud","Bioseguridad y Procedimientos Diagnósticos","Psicología de Atención"],
-  ["Integrado Fisio‑Farmacología 1","Salud Poblacional","Infectología Básica","Fundamentos de Física Médica","Matemática Integrada"],
+  ["Bioquímica General","Morfología Básica","Ética","Tecnología Médica en el Equipo de Salud","Bioseguridad y Procedimientos de Apoyo Diagnósticos","Psicología de Atención al paciente"],
+  ["Integrado Fisio‑Farmacología 1","Salud Poblacional","Infectología Básica","Fundamentos de Física Médica","Matemática Integrada a imagenología Médica"],
   ["Integrado Fisio‑Farmacología 2","Bioética","Epidemiología","Física Médica 1","Anatomía Imagenológica","Hito Evaluativo"],
   ["Persona y Sociedad","Informática Aplicada","Bioestadística","Física Médica 2","Técnicas Radiológicas 1","Anatomía Imagenológica Integrada"],
   ["Gestión en Equipos","Electivo 1 Formación","Radiobiología","Técnicas Radiológicas 2","Gestión de Calidad","Imagenología Patológica"],
   ["Electivo 2 Formación","Metodología Investigación","Medicina Nuclear","Ultrasonido","Tomografía Computada 1"],
-  ["Electivo 3","Tomografía Computada 2","Salud Digital","Radioterapia","Resonancia Magnética","Hito Evaluativo Interprofesional"],
-  ["Gestión Desarrollo Profesional","Análisis Clínico Integrado","Taller Investigación","Electivo A","Electivo B","Sistemas Acreditación"],
+  ["Electivo 3 Formación","Tomografía Computada 2","Salud Digital","Radioterapia","Resonancia Magnética","Hito Evaluativo Interprofesional"],
+  ["Gestión Desarrollo Profesional","Análisis Clínico Integrado","Taller Investigación","Electivo 1","Electivo 2","Sistemas Acreditación"],
   ["Internado"]
 ];
 
@@ -41,7 +41,7 @@ const prereq = {
   "Resonancia Magnética": ["Imagenología Patológica"],
   "Hito Evaluativo Interprofesional": ["Radiobiología","Técnicas Radiológicas 2"],
   "Análisis Clínico Integrado": ["Tomografía Computada 2","Resonancia Magnética"],
-  "Taller Investigación": ["Metodología Investigación"],
+  "Taller de Investigación": ["Metodología Investigación"],
   "Internado": ["Hito Evaluativo Interprofesional","Análisis Clínico Integrado"]
 };
 
